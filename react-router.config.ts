5207a6e298b7b8d068aa5f83c9768101d8a24327
onlyoneaman <2000.aman.sinha@gmail.com>
2025-04-02 18:52:00 +0530

    Add prerender links

--- react-router.config.ts
+++ react-router.config.ts
@@ -5,4 +5,9 @@ export default {
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
   tailwind: true,
+  async prerender() {
+    return [
+      "/", "/about"
+    ]
+  }
 } satisfies Config;
